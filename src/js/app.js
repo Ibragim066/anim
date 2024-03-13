a3b2d6e4262439c006ddb137069299013b4b07c0
Ibragim <gamemega.ao@gmail.com>
2024-03-13 19:36:43 +0300

    callback-chat and liker

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import Collapse from './Collapse/Collapse';
+import Modal from './Callback-Chat/Modal';
 
 const collapse = new Collapse(document.getElementById('collapse-container'));
+const modal = new Modal(document.getElementById('chat-container'));
 
 collapse.init();
+modal.init();
